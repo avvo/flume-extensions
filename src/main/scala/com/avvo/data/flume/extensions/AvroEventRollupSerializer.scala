94c4f5f22724e9dc30ce00d5cc8481f0d845df54
Steve Fosdal <sfosdal@avvo.com>
2017-07-24 14:52:33 -0700

    code reviewed in person. pushing discussed changes

--- src/main/scala/com/avvo/data/flume/extensions/AvroEventRollupSerializer.scala
+++ src/main/scala/com/avvo/data/flume/extensions/AvroEventRollupSerializer.scala
@@ -3,19 +3,19 @@ package com.avvo.data.flume.extensions
 import java.io._
 import java.nio.ByteBuffer
 
+import com.typesafe.scalalogging.LazyLogging
 import net.fosdal.oslo._
+import net.fosdal.oslo.oany._
+import org.apache.avro.Schema
 import org.apache.avro.file.{CodecFactory, DataFileStream, DataFileWriter}
 import org.apache.avro.generic.{GenericData, GenericDatumReader, GenericDatumWriter}
-import org.apache.avro.io.{DatumWriter, EncoderFactory}
-import org.apache.avro.{AvroRuntimeException, Schema}
+import org.apache.avro.io.EncoderFactory
 import org.apache.flume.conf.Configurable
 import org.apache.flume.serialization.AvroEventSerializerConfigurationConstants._
 import org.apache.flume.serialization.EventSerializer
 import org.apache.flume.{Context, Event, FlumeException}
 import org.apache.hadoop.conf.Configuration
 import org.apache.hadoop.fs.{FileSystem, Path}
-import org.slf4j.LoggerFactory
-
 
 /**
   * Serialize multiple Avro files using given schema based on the rollup size.
@@ -35,126 +35,106 @@ import org.slf4j.LoggerFactory
   * flume-ng agent -n flumeAgentName -f ./flume.config -C ./flume-extensions-assembly-0.1.jar
   */
 object AvroEventRollupSerializer {
-  private val logger = LoggerFactory.getLogger(classOf[AvroEventRollupSerializer])
+
+  val AvroSchemaPath = "avroSchemaPath"
 
   class Builder extends EventSerializer.Builder {
-    def build(context: Context, out: OutputStream): EventSerializer = {
-      using(new AvroEventRollupSerializer(out)) {
-        writer => {
-          writer.configure(context)
-          writer
-        }
+    override def build(context: Context, out: OutputStream): EventSerializer = {
+      new AvroEventRollupSerializer(out) tap {
+        _.configure(context)
       }
     }
   }
+
 }
 
-class AvroEventRollupSerializer(out: OutputStream) extends EventSerializer with Configurable {
-  val AVRO_SCHEMA_PATH: String = "avroSchemaPath"
+class AvroEventRollupSerializer(out: OutputStream) extends EventSerializer with Configurable with LazyLogging {
 
-  var compressionCodec: Option[String] = None
-  var dataFileWriter: Option[DataFileWriter[AnyRef]] = None
-  var schema: Option[Schema] = None
-  var schemaPath: Option[String] = None
-  var syncIntervalBytes: Integer = 0
-  var reader = new GenericDatumReader[AnyRef]
-  var writer: Option[DatumWriter[AnyRef]] = None
+  import AvroEventRollupSerializer._
 
-  def configure(context: Context): Unit = {
-    syncIntervalBytes = context.getInteger(SYNC_INTERVAL_BYTES, DEFAULT_SYNC_INTERVAL_BYTES)
-    compressionCodec = Some(context.getString(COMPRESSION_CODEC, DEFAULT_COMPRESSION_CODEC))
-    schemaPath = Some(context.getString(AVRO_SCHEMA_PATH))
-  }
+  var context: Option[Context] = None
+
+  lazy val compressionCodec  = context.get.getString(COMPRESSION_CODEC, DEFAULT_COMPRESSION_CODEC)
+  lazy val syncIntervalBytes = context.get.getInteger(SYNC_INTERVAL_BYTES, DEFAULT_SYNC_INTERVAL_BYTES)
+  lazy val schemaPath        = context.get.getString(AvroSchemaPath)
+  lazy val dataFileWriter    = initialize()
+
+  val reader = new GenericDatumReader[AnyRef]
+
+  override def configure(context: Context): Unit = this.context = Some(context)
 
   @throws[IOException]
-  def loadSchema(schemaFilePath: String): Option[Schema] = {
-    AvroEventRollupSerializer.logger.debug("Schema Path: " + schemaFilePath)
-
-    val parser = new Schema.Parser()
+  def loadSchema(schemaFilePath: String): Schema = {
+    logger.debug(s"""Schema Path: "$schemaFilePath"""")
     if (schemaFilePath.toLowerCase.startsWith("hdfs://")) {
-      val fs = FileSystem.get(new Configuration())
-      using(fs.open(new Path(schemaFilePath))) { input =>
-          Some(parser.parse(input))
+      using(FileSystem.get(new Configuration())) { fs =>
+        using(fs.open(new Path(schemaFilePath))) { is =>
+          val parser = new Schema.Parser()
+          parser.parse(is)
+        }
       }
     } else {
-      throw new FlumeException("Schema file path should begin with HDFS: " + schemaFilePath)
+      throw new FlumeException(s"""Schema file path should begin with HDFS: "$schemaFilePath"""")
     }
   }
 
   @throws[IOException]
-  def afterCreate() = {
-    // no-op
-  }
+  override def afterCreate(): Unit = ()
 
   @throws[IOException]
-  def afterReopen(): Unit = {
+  override def afterReopen(): Unit = {
     // impossible to initialize DataFileWriter without writing the schema?
     throw new UnsupportedOperationException("Avro API doesn't support append")
   }
 
   @throws[IOException]
   def serialize(value: AnyRef): Array[Byte] = {
-    using(new ByteArrayOutputStream()) {
-      outputStream => {
-        val encoder = EncoderFactory.get.directBinaryEncoder(outputStream, null)
-        val writer = new GenericDatumWriter[GenericData.Record](value.asInstanceOf[GenericData.Record].getSchema)
-        writer.write(value.asInstanceOf[GenericData.Record], encoder)
-        outputStream.toByteArray
-      }
+    using(new ByteArrayOutputStream()) { outputStream =>
+      val encoder = EncoderFactory.get.directBinaryEncoder(outputStream, null) // scalastyle:ignore
+      val writer  = new GenericDatumWriter[GenericData.Record](value.asInstanceOf[GenericData.Record].getSchema)
+      writer.write(value.asInstanceOf[GenericData.Record], encoder)
+      outputStream.toByteArray
     }
   }
 
   def readAvroData(flumeEventBody: Array[Byte]): Array[Byte] = {
     val inStream = new ByteArrayInputStream(flumeEventBody)
-    using(new DataFileStream[AnyRef](inStream, reader)) {
-      fileReader => serialize(fileReader.next)
+    using(new DataFileStream[AnyRef](inStream, reader)) { fileReader =>
+      serialize(fileReader.next)
     }
   }
 
   @throws[IOException]
-  private def initialize(event: Event) = {
+  private def initialize(): DataFileWriter[AnyRef] = {
     if (schemaPath.isEmpty) {
-      throw new FlumeException("schemaPath is not initialized.")
+      throw new FlumeException(s"""schemaPath is not valid: "$schemaPath""")
     }
-
-    schema = loadSchema(schemaPath.get)
-    writer = Some(new GenericDatumWriter[AnyRef](schema.get))
-
-    dataFileWriter = Some(new DataFileWriter(writer.get))
-    dataFileWriter.get.setSyncInterval(syncIntervalBytes)
+    val schema         = loadSchema(schemaPath)
+    val writer         = new GenericDatumWriter[AnyRef](schema)
+    val dataFileWriter = new DataFileWriter(writer)
+    dataFileWriter.setSyncInterval(syncIntervalBytes)
 
     try {
-      val codecFactory = CodecFactory.fromString(compressionCodec.get)
-      dataFileWriter.get.setCodec(codecFactory)
+      val codecFactory = CodecFactory.fromString(compressionCodec)
+      dataFileWriter.setCodec(codecFactory)
     } catch {
-      case e: AvroRuntimeException =>
-        AvroEventRollupSerializer.logger.warn("Unable to instantiate avro codec with name (" +
-          compressionCodec + "). Compression disabled. Exception follows.", e)
+      case e: Throwable =>
+        logger.warn(s"""Unable to instantiate avro codec with name, "$compressionCodec", compression disabled""", e)
     }
-
-    dataFileWriter.get.create(schema.get, out)
+    dataFileWriter.create(schema, out)
   }
 
   @throws[IOException]
-  def write(event: Event): Unit = {
-    if (dataFileWriter.isEmpty) {
-      initialize(event)
-    }
-    dataFileWriter.get.appendEncoded(ByteBuffer.wrap(readAvroData(event.getBody)))
+  override def write(event: Event): Unit = {
+    dataFileWriter.appendEncoded(ByteBuffer.wrap(readAvroData(event.getBody)))
   }
 
   @throws[IOException]
-  def flush(): Unit = {
-    if (dataFileWriter.isEmpty) {
-      throw new FlumeException("dataFileWriter is not initialized yet.")
-    }
-    dataFileWriter.get.flush()
-  }
+  override def flush(): Unit = dataFileWriter.flush()
 
   @throws[IOException]
-  def beforeClose(): Unit = {
-    // no-op
-  }
+  override def beforeClose(): Unit = ()
 
-  def supportsReopen: Boolean = false
+  override def supportsReopen: Boolean = false
+
 }
